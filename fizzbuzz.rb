03e29dba3599e609345b1d9f66d244a8e3ecdab1
Tak H Kim <takhyun818@gmail.com>
2018-08-02 21:30:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  end
-  if int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
-  end
-  if int % 3 && 5 == 0
+  elsif int % 3 && 5 == 0
     "FizzBuzz"
+  else 
+    "nil"
   end
 end
